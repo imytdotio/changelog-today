50c88e4296e21afb4e9de8d634225b3ff8a0ac61
imytdotio <imytdotio@gmail.com>
2023-01-17 08:02:26 +0800

    img styling

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -8,12 +8,12 @@ import React from "react";
 export const Profile = (props) => {
   return (
     <div className="w-full md:w-96 text-left m-auto">
-        <div className="flex h-28 mb-4 rounded-md bg-blue-200 p-2 border-box profile-card">
+      <div className="flex h-fit mb-4 rounded-md bg-blue-200 p-2 border-box profile-card">
         <img
           src={props.pfpUrl}
           alt="PFP"
           border="0"
-          className="rounded-full h-full mr-4 shadow-xl"
+          className="rounded-full h-24 mr-4 shadow-xl my-auto"
         />
         <div className="flex-col my-auto">
           <h1 className="font-bold text-2xl">
@@ -27,7 +27,9 @@ export const Profile = (props) => {
               @{props.userId}
             </span>
           </h1>
-          <p className="text-gray-600 text-sm w-full flex-1 text-white">{props.motto}</p>
+          <p className="text-gray-600 text-sm w-full flex-1 text-white">
+            {props.motto}
+          </p>
         </div>
       </div>
       <p>{props.selfIntro}</p>
